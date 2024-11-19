3b00d18c8f1abd5f068fcde3c5f47b214fb3e88f
Yousef Mostafa <yousef.com1162003@gmail.com>
2024-11-19 21:53:21 +0200

    Compare Signals function

--- Signals/QuanTest1.py
+++ Signals/QuanTest1.py
@@ -1,4 +1,4 @@
-def QuantizationTest1(file_name,Your_EncodedValues,Your_QuantizedValues):
+def QuantizationTest1(file_name,Your_EncodedValues,Your_QuantizedValues,For_Check=False):
     expectedEncodedValues=[]
     expectedQuantizedValues=[]
     with open(file_name, 'r') as f:
@@ -11,7 +11,10 @@ def QuantizationTest1(file_name,Your_EncodedValues,Your_QuantizedValues):
             L=line.strip()
             if len(L.split(' '))==2:
                 L=line.split(' ')
-                V2=str(L[0])
+                if For_Check :
+                    V2=int(L[0])
+                else:
+                    V2=str(L[0])
                 V3=float(L[1])
                 expectedEncodedValues.append(V2)
                 expectedQuantizedValues.append(V3)
@@ -19,16 +22,16 @@ def QuantizationTest1(file_name,Your_EncodedValues,Your_QuantizedValues):
             else:
                 break
     if( (len(Your_EncodedValues)!=len(expectedEncodedValues)) or (len(Your_QuantizedValues)!=len(expectedQuantizedValues))):
-        print("QuantizationTest1 Test case failed, your signal have different length from the expected one")
+        print(" Test case failed, your signal have different length from the expected one")
         return
     for i in range(len(Your_EncodedValues)):
         if(Your_EncodedValues[i]!=expectedEncodedValues[i]):
-            print("QuantizationTest1 Test case failed, your EncodedValues have different EncodedValues from the expected one") 
+            print(" Test case failed, your Values have different Values from the expected one") 
             return
     for i in range(len(expectedQuantizedValues)):
         if abs(Your_QuantizedValues[i] - expectedQuantizedValues[i]) < 0.01:
             continue
         else:
-            print("QuantizationTest1 Test case failed, your QuantizedValues have different values from the expected one") 
+            print(" Test case failed, your Values have different values from the expected one") 
             return
-    print("QuantizationTest1 Test case passed successfully")
+    print(" Test case passed successfully")
